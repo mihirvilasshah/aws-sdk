d6e635702ff5c726fe58bb618aae68ab94638802
Mihir <mihirhero2008@gmail.com>
2024-09-07 19:39:33 +0530

    uploadFileToS3

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 console.clear();
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 // console.log(process.env)
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-s3.listBuckets((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
+// s3.listBuckets((err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+
+const listBuckets = async () => {
+    try {
+        const data = await s3.listBuckets().promise();
         console.log(data);
+    } catch (error) {
+        console.log(error, error.stack);
     }
-})
+}
+
+const uploadFileToS3 = (filePath, bucketName) => {
+    const file = fs.readFileSync(filePath);
+    const params = {
+        Bucket: bucketName,
+        Key: path.basename(filePath),
+        Body: file,
+    };
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    });
+};
+
+listBuckets();
+uploadFileToS3('sample_files/example.txt', 'aws-sdk-node-app');
+uploadFileToS3('sample_files/file_example_JPG_100kB.jpg', 'aws-sdk-node-app');
